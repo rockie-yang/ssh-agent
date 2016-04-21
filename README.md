3a853907055433d440220abbb77b5299624d7737
Rockie Yang <eyouyan@gmail.com>
2016-04-21 22:52:16 +0200

    add simple ssh test to login to mac

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ then we can just start the app at http://localhost:3000
 ```shell
 	nodemon DEBUG=app npm start
 ```
+
+enable all users have ssh access to MAC
+```shell
+	dscl . change /Groups/com.apple.access_ssh RecordName com.apple.access_ssh com.apple.access_ssh-disabled
+```
+
--- routes/computers.js
+++ routes/computers.js
@@ -1,13 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var Client = require('ssh2').Client;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
 router.get('/:ip', function(req, res, next) {
+
+    var conn = new Client();
+    conn
+        .on('keyboard-interactive',
+            function(name, instructions, instructionsLang, prompts, finish) {
+                // Pass answers to `prompts` to `finish()`. Typically `prompts.length === 1`
+                // with `prompts[0] === "Password: "`
+                finish(['test']);
+            })
+        .on('ready', function() {
+            console.log('Client :: ready');
+            conn.exec('uptime', function(err, stream) {
+                if (err) throw err;
+                stream.on('close', function(code, signal) {
+                    console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+                    conn.end();
+                }).on('data', function(data) {
+                    console.log('STDOUT: ' + data);
+                }).stderr.on('data', function(data) {
+                    console.log('STDERR: ' + data);
+                });
+            });
+        })
+        .connect({
+            host: 'localhost',
+            port: 22,
+            username: 'test',
+            tryKeyboard: true
+        });
+
     console.log(req);
+
     res.send('respond with a ip');
 });
 
